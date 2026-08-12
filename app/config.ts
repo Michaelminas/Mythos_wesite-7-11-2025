ae92146dae4545d269bd705a4fcba7c7c1e539ed
mythosgreekentertainment-create <mythosgreekentertainment@gmail.com>
2026-08-12 18:22:30 +1000

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
-export const TICKET_URL = 'https://www.eventbrite.com/e/1987109182055?aff=oddtdtcreator'
+export const TICKET_URL = 'https://www.eventbrite.com.au/e/mythos-full-venue-takeover-rooftop-club-tickets-1997323816301?aff=ebdsoporgprofile'
 export const TICKET_DOMAIN = 'https://www.eventbrite.com'
 export const INSTAGRAM_EMBED_URL = 'https://www.instagram.com/p/DW8NuJ7EmMs/embed'
